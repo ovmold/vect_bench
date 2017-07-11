e9488e57c7c2cd1ffb21e762863b974251c49edd
ovmold <ovmold@gmail.com>
2017-07-11 18:57:24 +0700

    Options for running IACA for Broadwell added

--- run-iaca.sh
+++ run-iaca.sh
@@ -3,7 +3,10 @@
 # Intel Sandy Bridge
 #iaca -arch SNB -o iaca-out.out -graph iaca-out  ./matmul
 # Intel Haswell
-iaca -arch HSW -o ./iaca/iaca-out.out -graph ./iaca/iaca-out  ./matmul
+#iaca -arch HSW -o ./iaca/iaca-out.out -graph ./iaca/iaca-out  ./matmul
+
+# Intel Broadwell
+iaca -arch BDW -o ./iaca/iaca-out.out -graph ./iaca/iaca-out  ./matmul
 cd iaca
 dot -Tps ./iaca-out1.dot > ./iaca-out.ps
 
